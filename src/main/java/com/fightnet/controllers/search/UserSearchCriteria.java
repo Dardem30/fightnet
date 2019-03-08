e8d6316f4a4296b483f2d73a698b7f3c23ad0ece
Roman <rlukashenko@cygno.com>
2019-03-08 18:58:54 +0300

    Search by country/city

--- src/main/java/com/fightnet/controllers/search/UserSearchCriteria.java
+++ src/main/java/com/fightnet/controllers/search/UserSearchCriteria.java
@@ -1,12 +1,13 @@
 package com.fightnet.controllers.search;
 
 import lombok.Data;
+import org.bson.types.ObjectId;
 
 @Data
 public class UserSearchCriteria {
     private String name;
     private String surname;
     private String description;
-    private String country;
-    private String city;
+    private ObjectId country;
+    private ObjectId city;
 }
