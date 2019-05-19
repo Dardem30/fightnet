e3e402df6101724b77615d7cc4ef858279e9eeb8
Roman <rlukashenko@cygno.com>
2019-05-19 22:25:18 +0300

    send grid

--- src/main/java/com/fightnet/FightnetApplication.java
+++ src/main/java/com/fightnet/FightnetApplication.java
@@ -1,23 +1,14 @@
 package com.fightnet;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.Properties;
 
 @SpringBootApplication
 public class FightnetApplication {
     public static String facebookToken = "EAAFdvUOtnA8BAJEU1ZCjTRhtB9CAzYQFa3GO8AwQidpSUZCAGZAOTe2zJIyeCuEK3uZCMIpXLi55NqtvpkZCOi6DZBvu5mptZAuyQ4PhT9hbievaX2XayoPXgIMeqWItATm09WKLDnZCUywGyEQaJN4TwpXHfie09W4F4AUtIG1M92Ho67UZAR4p7swItsrYan3Xj5gzhJ89gUwZDZD";
-    @Value("${spring.mail.username}")
-    private String mailName;
-    @Value("${spring.mail.password}")
-    private String mailPassword;
 
     public static void main(String[] args) {
         SpringApplication.run(FightnetApplication.class, args);
@@ -26,25 +17,6 @@ public class FightnetApplication {
     @Bean(name = "passwordEncoder")
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        final JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setUsername(mailName);
-        mailSender.setPassword(mailPassword);
-
-        final Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        mailSender.setJavaMailProperties(props);
-
-        System.out.println(mailSender);
-        System.out.println(props);
-        System.out.println(mailName);
-        System.out.println(mailPassword);
-        return mailSender;
     }
 
     @Bean
--- src/main/java/com/fightnet/security/mail/EmailService.java
+++ src/main/java/com/fightnet/security/mail/EmailService.java
@@ -1,21 +1,20 @@
 package com.fightnet.security.mail;
 
+import com.sendgrid.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class EmailService {
-  
-    private final JavaMailSender emailSender;
- 
-    public void sendCodeMessage(final String to, final String subject, final String text) {
-        final SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to); 
-        message.setSubject(subject); 
-        message.setText(text);
-        emailSender.send(message);
+
+
+    public void sendCodeMessage(final String to, final String subject, final String text) throws Exception {
+        final SendGrid sg = new SendGrid("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");
+        final Request request = new Request();
+        request.method = Method.POST;
+        request.endpoint = "mail/send";
+        request.body = new Mail(new Email("fightseeker0@gmail.com"), subject, new Email(to), new Content("text/plain", text)).build();
+        sg.api(request);
     }
 }
