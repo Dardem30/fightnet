496996aff80511ab7b1eaa2a0645086ec3c75bb7
Roman <rlukashenko@cygno.com>
2019-06-05 18:12:49 +0300

    server url

--- src/main/java/com/fightnet/FightnetApplication.java
+++ src/main/java/com/fightnet/FightnetApplication.java
@@ -1,17 +1,13 @@
 package com.fightnet;
 
-import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class FightnetApplication {
-    private final MongoOperations operations;
     public static String facebookToken = "EAAFdvUOtnA8BAJEU1ZCjTRhtB9CAzYQFa3GO8AwQidpSUZCAGZAOTe2zJIyeCuEK3uZCMIpXLi55NqtvpkZCOi6DZBvu5mptZAuyQ4PhT9hbievaX2XayoPXgIMeqWItATm09WKLDnZCUywGyEQaJN4TwpXHfie09W4F4AUtIG1M92Ho67UZAR4p7swItsrYan3Xj5gzhJ89gUwZDZD";
 
     public static void main(String[] args) {
